8fabbaba9a967969cdeb0f22673f9e78c82546dd
reint <riff.sense@gmail.com>
2015-10-25 02:57:01 +0000

    bootstrap for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,104 @@
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
-
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bank Soal</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+        <style type='text/css'>
+                .form-signin
+            {
+                max-width: 330px;
+                padding: 15px;
+                margin: 0 auto;
+            }
+            .form-signin .form-signin-heading, .form-signin .checkbox
+            {
+                margin-bottom: 10px;
+            }
+            .form-signin .checkbox
+            {
+                font-weight: normal;
+            }
+            .form-signin .form-control
+            {
+                position: relative;
+                font-size: 16px;
+                height: auto;
+                padding: 10px;
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+            }
+            .form-signin .form-control:focus
+            {
+                z-index: 2;
+            }
+            .form-signin input[type="text"]
+            {
+                margin-bottom: -1px;
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+            }
+            .form-signin input[type="password"]
+            {
+                margin-bottom: 10px;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+            }
+            .account-wall
+            {
+                margin-top: 20px;
+                padding: 40px 0px 20px 0px;
+                background-color: #f7f7f7;
+                -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+            }
+            .login-title
+            {
+                color: #555;
+                font-size: 18px;
+                font-weight: 400;
+                display: block;
+            }
+            .profile-img
+            {
+                width: 96px;
+                height: 96px;
+                margin: 0 auto 10px;
+                display: block;
+                -moz-border-radius: 50%;
+                -webkit-border-radius: 50%;
+                border-radius: 50%;
+            }
+            .need-help
+            {
+                margin-top: 10px;
+            }
+            .new-account
+            {
+                display: block;
+                margin-top: 10px;
+            }
+            </style>
+        </script>
+    </head>
+<body>
+  <div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <h1 class="text-center login-title">APLIKASI BANK SOAL</h1>
+            <div class="account-wall">
+                <img class="profile-img" src="{{ asset('gambar/photo.png') }}" size="50">
+                <form class="form-signin" method="POST" action="/auth/login">
+                {!! csrf_field() !!}
+                <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus><span class="clearfix"></span>
+                <input type="password" class="form-control" placeholder="Password" name="password" id="password">
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in</button><span class="clearfix"></span>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
-
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
+</div>
+</body>
+</html>
